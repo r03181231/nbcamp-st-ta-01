849e627911a18327a2489a7a7f212241d658f78e
HaeRheeNam <4ran0318@gmail.com>
2024-02-06 15:07:48 +0900

    [test01]

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
+import { useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import { onMinusNum, onPlusNum, onTotalNum } from "./redux/modules/calculator";
 
 function App() {
+  const dispatch = useDispatch();
+  const [number, setNumber] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const onChangeValue = (e) => {
+    setNumber(+e.target.value);
+    setTotal(total);
+  };
+  const onClickPlusNum = (number) => {
+    console.log(number);
+    dispatch(onPlusNum(number));
+    dispatch(onTotalNum(number));
+  };
+  const onClickMinusNum = (number) => {
+    console.log(number);
+    dispatch(onMinusNum(number));
+    dispatch(onTotalNum(number));
+  };
+
   return (
     <div className="App">
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input
+          type="number"
+          name="number"
+          value={number}
+          onChange={onChangeValue}
+        />{" "}
+        만큼을 <button onClick={() => onClickPlusNum(number)}>더할게요</button>{" "}
+        <button onClick={() => onClickMinusNum(number)}>뺄게요</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{total}</p>
       </div>
     </div>
   );
--- src/redux/modules/calculator.js
+++ src/redux/modules/calculator.js
@@ -1,10 +1,52 @@
+// action value
+const PLUS_NUM = "PLUS_NUM";
+const MINUS_NUM = "MINUS_NUM";
+const TOTAL_NUM = "TOTAL_NUM";
+
+export const onPlusNum = (payload) => {
+  return {
+    type: PLUS_NUM,
+    payload,
+  };
+};
+export const onMinusNum = (payload) => {
+  return {
+    type: MINUS_NUM,
+    payload,
+  };
+};
+
+export const onTotalNum = () => {
+  return {
+    type: TOTAL_NUM,
+  };
+}; // action creator
+
 // 초기 상태값
 const initialState = {
   number: 0,
+
+  totalNumber: 0,
 };
 
 // 리듀서
-const calculator = (state = initialState, action) => {};
+const calculator = (state = initialState, action) => {
+  console.log("action.payload=>", action.payload);
+  console.log(state.number);
+  switch (action.type) {
+    case "PLUS_NUM":
+      return { ...state, number: state.number + action.payload };
+    case "MINUS_NUM":
+      return { ...state, number: state.number - action.payload };
+    case "TOTAL_NUM":
+      return {
+        totalNumber:
+          state.number + action.payload + (state.number - action.payload),
+      };
+    default:
+      return state;
+  }
+};
 
 // 모듈파일에서는 리듀서를 export default 한다.
 export default calculator;
